b6bad294fd578bb667be571c205153afa5bef04f
Matt <matt@isprinklr.lan>
2023-07-06 00:49:08 +0000

    API complete, needs linkage to sprinkler_serial

--- api.py
+++ api.py
@@ -35,37 +35,51 @@ sprinklr_running = False
 active_sprinklr = None
 system_error = False
 end_time = None
+sprinklr_task = None
 
 async def run_sprinklr(sprinklr: int,duration: int):
     global sprinklr_running, active_sprinklr, end_time
     sprinklr_running = True
     active_sprinklr = sprinklr
-    end_time = time.time() + duration
-    await asyncio.sleep(duration)  # Simulate a long-running process with a sleep for the given duration
+    end_time = time.time() + duration*60
+    logger.debug(f"Starting sprinkler {active_sprinklr}, ending at {end_time}")
+    await asyncio.sleep(duration*60)  # Simulate a long-running process with a sleep for the given duration
     sprinklr_running = False
 
 @app.get("/api/start_sprinklr/{sprinklr}/duration/{duration}")
 async def start_sprinklr(sprinklr: int, duration: int):
-    global sprinklr_running, system_error
+    global sprinklr_running, system_error, sprinklr_task, active_sprinklr, end_time
     if system_error:
-        return {"message": "System Error"}
+        return {"message": "System Error", "systemStatus": "error"}
     if not sprinklr_running:
-        asyncio.create_task(run_sprinklr(sprinklr, duration))  # Start the long-running process in the background
-        return {"message": f"Started sprinkler {sprinklr} for {duration} seconds"}
+        sprinklr_task = asyncio.create_task(run_sprinklr(sprinklr, duration))  # Start the long-running process in the background
+        return {"message": f"Started sprinkler {sprinklr} for {duration} minutes", "systemStatus": "active"}
     else:
-        return {"message": "System Running"}
+        return {"message": "Error: system already active", "zone": active_sprinklr, "duration": math.ceil(end_time - time.time()), "systemStatus": "active"}
+
+@app.get("/api/stop_sprinklr/")
+def stop_sprinklr():
+    global sprinklr_running, system_error, end_time, active_sprinklr, sprinklr_task
+    if system_error:
+        return {"message": "System Error", "systemStatus": "error"}
+    # stop the sprinklr
+    sprinklr_task.cancel()
+    sprinklr_running = False
+    end_time = 0
+    return {"message": f"{active_sprinklr} stopped", "systemStatus": "inactive"}
+
 
 @app.get("/api/status")
 def get_status():
     global sprinklr_running, active_sprinklr, system_error
     if system_error:
-        return {"duration": -1, "message": "System Error"}
+        return {"duration": -1, "message": "System Error", "systemStatus": "error"}
     if sprinklr_running:
-        logger.debug('Test')
         logger.debug('Active Sprinklr %s', active_sprinklr)
-        return {"message": f"Zone: {active_sprinklr} running", "zone": active_sprinklr, "duration": math.ceil(end_time - time.time())}
+        return {"systemStatus": "active", "message": f"Zone: {active_sprinklr} running", "zone": active_sprinklr, "duration": math.ceil(end_time - time.time())}
     else:
-        return {"duration": 0, "message": "System Idle"}
+        logger.debug('System Idle')
+        return {"duration": 0, "message": "System inactive", "systemStatus": "inactive"}
 
 # api route to return the list of sprinklers
 @app.get("/api/sprinklers")
