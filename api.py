12c4a612284e05b46ba5c7cd8a5d9e98f940299d
Matt <matt@isprinklr.lan>
2023-07-08 19:52:09 +0000

    Adds error handling for serial port connections

--- api.py
+++ api.py
@@ -1,12 +1,13 @@
 import asyncio, pandas as pd, logging, time, math
+import sprinklr_serial as hunterserial
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
 logging.basicConfig(filename="api.log",
                     format='%(asctime)s %(message)s',
-                    filemode='w')
+                    filemode='w',
+                    level=logging.DEBUG)
 logger = logging.getLogger()
-logger.setLevel(logging.DEBUG)
 
 app = FastAPI()
 
@@ -43,8 +44,14 @@ async def run_sprinklr(sprinklr: int,duration: int):
     active_sprinklr = sprinklr
     end_time = time.time() + duration*60
     logger.debug(f"Starting sprinkler {active_sprinklr}, ending at {end_time}")
-    await asyncio.sleep(duration*60)  # Simulate a long-running process with a sleep for the given duration
-    sprinklr_running = False
+    try:
+        hunterserial.start_zone(sprinklr, duration)
+        await asyncio.sleep(duration*60)  # Simulate a long-running process with a sleep for the given duration
+        sprinklr_running = False
+    except IOError as exc:
+        logger.debug(f"Caught file I/O error {str(exc)}")
+        system_error = True
+        raise exc
 
 @app.get("/api/start_sprinklr/{sprinklr}/duration/{duration}")
 async def start_sprinklr(sprinklr: int, duration: int):
@@ -52,8 +59,11 @@ async def start_sprinklr(sprinklr: int, duration: int):
     if system_error:
         return {"message": "System Error", "systemStatus": "error"}
     if not sprinklr_running:
-        sprinklr_task = asyncio.create_task(run_sprinklr(sprinklr, duration))  # Start the long-running process in the background
-        return {"message": f"Started sprinkler {sprinklr} for {duration} minutes", "systemStatus": "active"}
+        try:
+            sprinklr_task = asyncio.create_task(run_sprinklr(sprinklr, duration))  # Start the long-running process in the background
+            return {"message": f"Started sprinkler {sprinklr} for {duration} minutes", "systemStatus": "active"}
+        except IOError as exc:
+            return {"message": "Error: Serial Port error", "systemStatus": "error"}
     else:
         return {"message": "Error: system already active", "zone": active_sprinklr, "duration": math.ceil(end_time - time.time()), "systemStatus": "active"}
 
@@ -63,10 +73,17 @@ def stop_sprinklr():
     if system_error:
         return {"message": "System Error", "systemStatus": "error"}
     # stop the sprinklr
-    sprinklr_task.cancel()
-    sprinklr_running = False
-    end_time = 0
-    return {"message": f"{active_sprinklr} stopped", "systemStatus": "inactive"}
+    logger.debug(f"Stopping sprinkler {active_sprinklr}")
+    try:
+        hunterserial.stop_zone(active_sprinklr)
+        sprinklr_task.cancel()
+        sprinklr_running = False
+        end_time = 0
+        return {"message": f"{active_sprinklr} stopped", "systemStatus": "inactive"}
+    except IOError as exc:
+        logger.debug(f"Caught file I/O error {str(exc)}")
+        system_error = True
+        return {"message": "Error: Serial Port error", "systemStatus": "error"}
 
 
 @app.get("/api/status")
--- sprinklr_serial.py
+++ sprinklr_serial.py
@@ -1,9 +1,21 @@
 #!/usr/bin/env python3
 import serial
 import time
+import logging
 
-# ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
+""" Command format:
+- Send 0xFE to initiate command
+- Terminate command with 0xFF
+- Actual command is 3 bytes
+- First byte is an integer with values of 1, 2, or 3
+	1: Start zone, requires other 2 bytes to specify zone 
+	and duration
+	2: Stop zone, 2nd byte is zone, 3rd byte is ignored
+	3: run program. Not yet implemented """
+
+# Serial ports:
 # /dev/tty*** for production, /dev/pts/* for virtual serial testing
+
 
 
 def start_zone(zone, duration):
@@ -38,23 +50,31 @@ def stop_zone(zone):
 
 def write_to_serial(cmd):
     try:
-        arduino = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
-        print(arduino)
+        arduino = serial.Serial('/dev/ttyACM1', 9600, timeout=1)
         arduino.flush()
         arduino.write(cmd)
+        logging.debug(f'Wrote {cmd} to serial')
         time.sleep(0.05)
-        data = arduino.readline()
-        print(data)
+        while(arduino.inWaiting() > 0):
+            data = arduino.readline().decode('utf-8').rstrip()
+            logging.debug(data)
+            time.sleep(0.05)
         arduino.close()
+        logging.debug('Closed serial connection')
 
     except IOError as exc:
-        print('Caught file I/O error' + str(exc))
+        logging.debug(f'Caught file I/O error {str(exc)}')
         raise exc
 
 if __name__ == "__main__":
+    logging.basicConfig(filename="serial.log",
+                        format='%(asctime)s %(message)s',
+                        filemode='w')
+    logger = logging.getLogger()
+    logger.setLevel(logging.DEBUG)
     cmd = (
         bytes([254])
-        + bytes([1])
+        + bytes([6])
         + bytes([int(8)])
         + bytes([int(2)])
         + bytes([255])
@@ -64,7 +84,9 @@ if __name__ == "__main__":
     print(ser.name)
     ser.write(cmd)
     time.sleep(0.05)
-    data = ser.readline().decode('utf-8').rstrip()
-    print(data)
+    while(ser.inWaiting() > 0):
+        data = ser.readline().decode('utf-8').rstrip()
+        print(data)
+        time.sleep(0.05)
     ser.close
 
